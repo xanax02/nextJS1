74579e1334b0e1d0472dbddb34873ba4b60aa0b5
abhay <abhaythakur00astro@gmail.com>
2022-10-15 02:04:01 +0530

    pre-rendered homePage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,38 @@
-import { getFeaturedEvents } from "../dummy-data";
+// import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/EventList";
 
-const HomePage = () => {
-  const events = getFeaturedEvents();
+const HomePage = (props) => {
+  const { featuredEvents } = props;
 
   return (
     <div>
-      <EventList events={events} />
+      <EventList events={featuredEvents} />
     </div>
   );
 };
 
 export default HomePage;
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://next-js-22e3a-default-rtdb.firebaseio.com/events.json"
+  );
+  const data = await response.json();
+
+  const loadedEvents = [];
+
+  for (const key in data) {
+    loadedEvents.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  const featuredEvents = loadedEvents.filter((event) => event.isFeatured);
+
+  return {
+    props: {
+      featuredEvents: featuredEvents,
+    },
+  };
+}
