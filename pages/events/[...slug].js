ef9db2a9e961efd610cdfd752e2dc5cb75ab5e22
abhay <abhaythakur00astro@gmail.com>
2022-10-15 17:25:12 +0530

    client-side-rendering for slug-page of events

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,69 +1,39 @@
+import { useEffect } from "react";
+import useSWR from "swr";
 import { useRouter } from "next/router";
 import EventList from "../../components/events/EventList";
 import { getFilteredEvents } from "../../dummy-data";
 
-const FilteredEventsPage = (props) => {
-  // const router = useRouter();
-  // const data = router.query.slug;
+//CLIENT SIDE RENDERING
+const FilteredEventsPage = () => {
+  const router = useRouter();
+  const date = router.query.slug;
 
-  // if (!data) {
-  //   return <p style={{ textAlign: "center" }}>Loading...</p>;
-  // }
+  console.log(date);
 
-  // const numYear = +data[0];
-  // const numMonth = +data[1];
+  if (!date) {
+    return <p style={{ textAlign: "center" }}>Loading...</p>;
+  }
 
-  // if (
-  //   isNaN(numYear) ||
-  //   isNaN(numMonth) ||
-  //   numMonth < 1 ||
-  //   numMonth > 12 ||
-  //   numYear < 2020 ||
-  //   numYear > 2030
-  // ) {
-  //   return <p>invalid filter option</p>;
-  // }
+  const numYear = +date[0];
+  const numMonth = +date[1];
 
-  if (props.hasError) {
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numMonth < 1 ||
+    numMonth > 12 ||
+    numYear < 2020 ||
+    numYear > 2030
+  ) {
     return <p>invalid filter option</p>;
   }
 
-  // const filteredEvents = getFilteredEvents({
-  //   year: numYear,
-  //   month: numMonth,
-  // });
-
-  console.log(props.filteredEvent);
-
-  if (!props.filteredEvent || props.filteredEvent.length === 0) {
-    return <p>No events found for the chosen filter!</p>;
-  }
-
-  return (
-    <>
-      <EventList events={props.filteredEvent} />
-    </>
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error } = useSWR(
+    "https://next-js-22e3a-default-rtdb.firebaseio.com/events.json",
+    fetcher
   );
-};
-export default FilteredEventsPage;
-///////
-/////////
-///////
-///////
-////////
-//////////
-////////
-///////////
-///////////////
-//
-
-// SSR
-
-export async function getServerSideProps(context) {
-  const response = await fetch(
-    "https://next-js-22e3a-default-rtdb.firebaseio.com/events.json"
-  );
-  const data = await response.json();
 
   const allEvents = [];
 
@@ -73,10 +43,6 @@ export async function getServerSideProps(context) {
       ...data[key],
     });
   }
-
-  const date = context.params.slug;
-  const numYear = +date[0];
-  const numMonth = +date[1];
 
   const filteredEvent = [];
 
@@ -90,9 +56,75 @@ export async function getServerSideProps(context) {
     }
   });
 
-  return {
-    props: {
-      filteredEvent,
-    },
-  };
-}
+  if (!filteredEvent || error) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
+  return (
+    <>
+      <EventList events={filteredEvent} />
+    </>
+  );
+};
+
+export default FilteredEventsPage;
+
+///SERVER SIDE RENDERING
+// const FilteredEventsPage = (props) => {
+
+//   if (props.hasError) {
+//     return <p>invalid filter option</p>;
+//   }
+
+// if (!props.filteredEvent || props.filteredEvent.length === 0) {
+//   return <p>No events found for the chosen filter!</p>;
+// }
+
+// return (
+//   <>
+//     <EventList events={props.filteredEvent} />
+//   </>
+// );
+// };
+// export default FilteredEventsPage;
+
+//////////////////////////////
+
+// SSR
+// export async function getServerSideProps(context) {
+//   const response = await fetch(
+//     "https://next-js-22e3a-default-rtdb.firebaseio.com/events.json"
+//   );
+//   const data = await response.json();
+
+//   const allEvents = [];
+
+//   for (const key in data) {
+//     allEvents.push({
+//       id: key,
+//       ...data[key],
+//     });
+//   }
+
+//   const date = context.params.slug;
+//   const numYear = +date[0];
+//   const numMonth = +date[1];
+
+//   const filteredEvent = [];
+
+//   allEvents.find((event) => {
+//     const eventDate = new Date(event.date);
+//     if (
+//       eventDate.getFullYear() === numYear &&
+//       eventDate.getMonth() === numMonth - 1
+//     ) {
+//       filteredEvent.push(event);
+//     }
+//   });
+
+//   return {
+//     props: {
+//       filteredEvent,
+//     },
+//   };
+// }
