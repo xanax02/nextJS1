fb5a75749d82bd0024e89cd3ae3a45d910157d16
abhay <abhaythakur00astro@gmail.com>
2022-10-28 22:15:13 +0530

    added get request for feedback

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,5 +1,20 @@
 import fs from "fs";
 import path from "path";
+
+//utility functions//
+//filePath function
+const getFilePath = () => {
+  const filePath = path.join(process.cwd(), "data", "feedback.json");
+  return filePath;
+};
+
+//fileData function for getting data from backend or file(here).
+const fileData = (filePath) => {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+};
+
 // this is not a component this is a function (server side code) for handling requests
 function handler(req, res) {
   if (req.method === "POST") {
@@ -12,14 +27,19 @@ function handler(req, res) {
       feedback: feedbackText,
     };
 
-    const filePath = path.join(process.cwd(), "data", "feedback.json");
-    const fileData = fs.readFileSync(filePath); // first we'll have the data from the feedback json file;
-    const data = JSON.parse(fileData); // converting the json data into normal form
+    ////this has been move to the utility functions
+    // const filePath = path.join(process.cwd(), "data", "feedback.json");
+    // const fileData = fs.readFileSync(filePath); // first we'll have the data from the feedback json file;
+    // const data = JSON.parse(fileData); // converting the json data into normal form
+    const filePath = getFilePath();
+    const data = fileData(filePath);
     data.push(newFeedback); // adding new feedback to the data.
     fs.writeFileSync(filePath, JSON.stringify(data)); // now add this new  data file with new feedback to the feedback.json file.
     res.status(201).json({ message: "Success", feedback: newFeedback });
   } else {
-    res.status(200).json({ message: "This works!" }); // .json to set the data that will be sent when a request is made
+    const filePath = getFilePath();
+    const data = fileData(filePath);
+    res.status(200).json({ feedback: data }); // .json to set the data that will be sent when a request is made
   }
 }
 
