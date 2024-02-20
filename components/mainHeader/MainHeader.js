8a6a3c48e6b1defe5602060c821f0c63c06ede53
abhay <abhaytxanax@gmail.com>
2024-02-21 00:49:11 +0530

    css-modules

--- components/mainHeader/MainHeader.js
+++ components/mainHeader/MainHeader.js
@@ -1,16 +1,16 @@
 import Link from "next/link";
-
 import LogoPng from '@/assets/logo.png'
+import styles from "./MainHeader.module.css"
 
 export default function MainHeader() {
     return (
-        <header>
-            <Link href='/'>
+        <header className={styles.header}>
+            <Link href='/' className={styles.logo}>
                 <img src={LogoPng.src} alt={"A plate with food on it"} />
                 NextLevel Food
             </Link>
 
-            <nav>
+            <nav className={styles.nav}>
                 <ul>
                     <li>
                         <Link href={"/meals"}>Browse Meals</Link>
