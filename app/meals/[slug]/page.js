0fb6930f803ace4bf378dec5a9a8272cc0b196d7
abhay <abhaytxanax@gmail.com>
2024-03-06 23:21:56 +0530

    slug and notFound function of next/navigation

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,5 +1,37 @@
-export default function SomeSlugPage() {
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+import { getMeal } from '@/lib/meals';
+import styles from './page.module.css';
+
+export default function MealDetailsPage({params}) {
+
+    const meal = getMeal(params.slug);
+    meal.instructions = meal?.instructions?.replace(/\n/g, '</br>');
+
+    if(!meal) {
+        notFound();
+    }
+
     return (
-        <h1>Some Slug Page</h1>
+       <>
+         <header className={styles.header}>
+            <div className={styles.image}>
+                <Image src={meal.image} alt={meal.title} fill />
+            </div>
+            <div className={styles.headerText}>
+                <h1>{meal.title}</h1>
+                <p className={styles.creator}>
+                    by <a href={`mail to ${meal.creator_email}`}>{meal.creator}</a>
+                </p>
+                <p className={styles.summary}>{meal.summary}</p>
+            </div>
+        </header>
+        <main>
+            <p className={styles.instructions} 
+            dangerouslySetInnerHTML={{__html: meal.instructions,}}
+            ></p>
+        </main>
+       </>
     )
 }
