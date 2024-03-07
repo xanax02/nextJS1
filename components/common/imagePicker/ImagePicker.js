0c0f448a6c781651a3309f7a12ecb476213615d4
abhay <abhaytxanax@gmail.com>
2024-03-07 23:40:32 +0530

    image picker

--- components/common/imagePicker/ImagePicker.js
+++ components/common/imagePicker/ImagePicker.js
@@ -1,9 +1,17 @@
 import styles from './ImagePicker.module.css'
 
-export default function ImagePicker({label}) {
+export default function ImagePicker({label, name}) {
     return (
         <div className={styles.picker}>
-            <label></label>
+            <label htmlFor={name}>{label}</label>
+            <div className={styles.controls}>
+                <input
+                    type='file'
+                    id={name}
+                    accept='image/png, image/jpeg'
+                    name={name}
+                />
+            </div>
         </div>
     )
 }
