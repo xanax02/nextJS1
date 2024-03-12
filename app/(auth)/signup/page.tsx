b4bd55908b0bcb1beb05760d74405a1bd0a8b64e
abhay <abhaytxanax@gmail.com>
2024-03-13 03:41:42 +0530

    backendRoute

--- app/(auth)/signup/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Signup() {
-    return <h1>Hello world</h1>
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
-import Image from "next/image";
+import axios from "axios";
 
-export default function Home() {
+async function getUserData() {
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  return response.data;
+}
+
+export default async function Home() {
+
+  const userData = await getUserData();;
+
   return (
-    <div>Hi There</div>
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
+    </div>
   );
 }
