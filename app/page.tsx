1596ec6f328cb2a47dc5edac3569632fed433379
abhay <abhaytxanax@gmail.com>
2024-03-14 00:05:00 +0530

    signup page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 async function getUserData() {
-  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  const response = await axios.get("http://localhost:3000/api/user")
   return response.data;
 }
 
